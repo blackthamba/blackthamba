2be7f817659a96cf972c8a785f802426696be60e
João Maciel <jhsmaciel15@gmail.com>
2025-07-25 18:06:04 -0300

    Refactor App component layout to use MUI Box and Container for improved styling and structure, enhancing the homepage presentation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import AppBar from 'components/appbar';
 import BlackThambaLandingPage from 'pages/home';
+import { Box, Container } from '@mui/material';
 
 function App() {
   return (
     <div>
       <AppBar />
-      <BlackThambaLandingPage />
+      <Box
+        className='wrapper'
+        sx={{
+          bgcolor: 'background.default',
+          color: 'text.primary',
+          width: '100%',
+          minHeight: '100vh'
+        }}
+      >
+        <Container
+          maxWidth="xl"
+          className='wrapper'
+          sx={{
+            bgcolor: 'background.default',
+            color: 'text.primary',
+          }}
+        >
+          <BlackThambaLandingPage />
+        </Container>
+      </Box>
     </div>
-      
   );
 }
 
